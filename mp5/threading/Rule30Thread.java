8ad53472faf3ee6b3974d8f78a1c6b53adf40f9e
Tim <timmyrino@gmail.com>
2016-12-07 22:21:38 +0800

    Add files via upload

--- mp5/threading/Rule30Thread.java
+++ mp5/threading/Rule30Thread.java
@@ -2,18 +2,18 @@ package threading;
 
 public class Rule30Thread extends Thread{
 
-	private static String first;
+	private static String input;
     private int min;
     private int max;
     private String ans = "";
     
-    public Rule30Thread(int min, int max, String first) {
+    public Rule30Thread(int min, int max, String input) {
         if (min > max || min < 0 || max < 0) {
             throw new IllegalArgumentException("error");
         }
         this.min = min;
         this.max = max;
-        this.first = first;
+        this.input = input;
     }
     public void run(){
     	 for(int i = min; i <= max; i++){
@@ -25,29 +25,29 @@ public class Rule30Thread extends Thread{
 		char ans ='0';
 		
 		if(i == 0){
-	        if(first.charAt(i + 1) == '1' && first.charAt(i) == '1')
+	        if(input.charAt(i + 1) == '1' && input.charAt(i) == '1')
 	            ans = '1';
-	        if(first.charAt(i) == '1' &&  first.charAt(i + 1) != '1')
+	        if(input.charAt(i) == '1' &&  input.charAt(i + 1) != '1')
 	            ans = '1';
-	        if(first.charAt(i) != '1' && first.charAt(i + 1) == '1')
+	        if(input.charAt(i) != '1' && input.charAt(i + 1) == '1')
 	            ans = '1';  
 	        }
 	        
-	        else if(i == first.length() - 1 ){
-	        if(first.charAt(i - 1) == '1' && first.charAt(i) != '1')
+	        else if(i == input.length() - 1 ){
+	        if(input.charAt(i - 1) == '1' && input.charAt(i) != '1')
 	            ans = '1';
-	        if(first.charAt(i - 1) != '1' && first.charAt(i) == '1')
+	        if(input.charAt(i - 1) != '1' && input.charAt(i) == '1')
 	            ans = '1';
 	        }
 	        
 	        else{
-	        if(first.charAt(i - 1) == '1' && first.charAt(i) != '1' && first.charAt(i + 1) != '1')
+	        if(input.charAt(i - 1) == '1' && input.charAt(i) != '1' && input.charAt(i + 1) != '1')
 	            ans = '1';
-	        if(first.charAt(i - 1) != '1' && first.charAt(i) == '1' && first.charAt(i + 1) == '1')
+	        if(input.charAt(i - 1) != '1' && input.charAt(i) == '1' && input.charAt(i + 1) == '1')
 	            ans = '1';
-	        if(first.charAt(i - 1) != '1' && first.charAt(i) == '1' &&  first.charAt(i + 1) != '1')
+	        if(input.charAt(i - 1) != '1' && input.charAt(i) == '1' &&  input.charAt(i + 1) != '1')
 	            ans = '1';
-	        if(first.charAt(i - 1) != '1' && first.charAt(i) != '1' && first.charAt(i + 1) == '1')
+	        if(input.charAt(i - 1) != '1' && input.charAt(i) != '1' && input.charAt(i + 1) == '1')
 	            ans = '1';  
 	        }
 	        
